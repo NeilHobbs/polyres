a4e8624e949d57a859aa8c05b69bf5b8085eca20
NeilHobbs <neilphobbs@hotmail.com>
2020-05-18 16:20:13 +0100

    With :: and imports

--- R/plot_resistance_to_bioassay_survival.R
+++ R/plot_resistance_to_bioassay_survival.R
@@ -1,3 +1,12 @@
+#'
+#' @import magrittr dplyr ggplot2
+#' @importFrom magrittr %>%
+#' @name %>%
+#' 
+#' @export
+#'
+
+
 plot_resistance_to_bioassay_survival = function(maximum.bioassay.survival.proportion, 
                                                 michaelis.menton.slope, 
                                                 half.population.bioassay.survival.resistance, 
@@ -6,9 +15,9 @@ plot_resistance_to_bioassay_survival = function(maximum.bioassay.survival.propor
                                                 maximum.resistance, 
                                                 sd.population.resistance){
   
-  df=data.frame(resistance.values=seq(minimum.resistance, maximum.resistance, by = 1))%>%
-    rowwise%>%
-    mutate(bioassay.survival.proportion = resistance_to_bioassay_survival(
+    df=data.frame(resistance.values=seq(minimum.resistance, maximum.resistance, by = 1))%>%
+    dplyr::rowwise()%>%
+    dplyr::mutate(bioassay.survival.proportion = resistance_to_bioassay_survival(
       maximum.bioassay.survival.proportion = maximum.bioassay.survival.proportion, 
       mean.population.resistance = resistance.values, 
       michaelis.menton.slope=michaelis.menton.slope, 
@@ -16,9 +25,10 @@ plot_resistance_to_bioassay_survival = function(maximum.bioassay.survival.propor
       sd.population.resistance = sd.population.resistance,
       nsim=nsim)) ##plotting with sigma as 0.1 or 25 made no difference to plots
   
-  ggplot(df, aes(x=resistance.values, y = bioassay.survival.proportion)) + ##logging to increase ease of readibility
+    ggplot(df, aes(x=resistance.values, y = bioassay.survival.proportion)) + ##logging to increase ease of readibility
     geom_point(colour = "blue") +
     xlab("Insecticide Resistance") + ##applying log(z) makes it more readable at the lower z levels.
     ylab("Bioassay Survival Proportion")+ #note: this label may need to be changed depending on what we calibrate against
     theme_classic()
 }
+
