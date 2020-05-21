63af260d513866ea88394a7239b427c1fc3e5e6a
NeilHobbs <neilphobbs@hotmail.com>
2020-05-21 15:21:26 +0100

    First upload.

---
+++ R/plot_log_response_to_insecticide_selection.R
@@ -0,0 +1,26 @@
+#' Function to plot a histogram of the response_to_insecticide_selection output.
+#' Plot to give years to 10% Survival (assuming 10 generations per year)
+#'
+#' @import ggplot2
+#' 
+#' @param response.values As obtained from the response_to_insecticide_selection function
+
+plot_log_response_to_insecticide_selection = function(response.values){
+  
+  response.values = log10(response.values)
+  
+  #Coerce into a dataframe to allow for plotting
+  output=data.frame(response.values) 
+  
+  #Histogram of years
+  selection.histogram =  ggplot(data = output, aes(x=output)) +
+    geom_histogram(fill = "grey", colour = "black") +
+    xlab("log(Years to 10% Survival)") +
+    theme_classic()
+  
+  return(selection.histogram)
+}
+
+
+
+
