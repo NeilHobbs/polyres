f77bd351fcd22319240e68cfbbf8d2070a140f70
NeilHobbs <neilphobbs@hotmail.com>
2020-05-21 15:26:22 +0100

    Default values now included

--- R/plot_bioassay_survival_to_resistance.R
+++ R/plot_bioassay_survival_to_resistance.R
@@ -12,7 +12,7 @@
 #' @param sd.population.resistance How much variation in the population resistance. 
 #' @param nsim How many replications of the rnorm function are conducted. Recommended value is 1000.
 #' @param minimum.resistance.value Recommend setting as 0.
-#' @param maximum.resistance.value This will depend on the half survival scale, but 10000 would be a good start. 
+#' @param maximum.resistance.value This will depend on the half survival scale, but 25000 would be a good start. 
 #' @param minimum.bioassay.survival As a proportion, should be set as 0
 #' @param maximum.bioassay.survival As a proportion, should be set as 1
 #' @param divisions The resolution of the final plotted graph
@@ -29,22 +29,22 @@
 #' sd.population.resistance = 10, 
 #' nsim = 1000, 
 #' minimum.resistance.value = 0, 
-#' maximum.resistance.value = 10000,
+#' maximum.resistance.value = 25000,
 #' minimum.bioassay.survival = 0, 
 #' maximum.bioassay.survival = 1, 
 #' divisions = 0.01)
 
-plot_bioassay_survival_to_resistance = function(maximum.bioassay.survival.proportion,
-                                                michaelis.menten.slope, 
-                                                half.population.bioassay.survival.resistance, 
-                                                estimate.precision, 
+plot_bioassay_survival_to_resistance = function(maximum.bioassay.survival.proportion = 1,
+                                                michaelis.menten.slope = 1, 
+                                                half.population.bioassay.survival.resistance = 900, 
+                                                estimate.precision = 0.01, 
                                                 sd.population.resistance, 
-                                                nsim, 
-                                                minimum.resistance.value, 
-                                                maximum.resistance.value,
-                                                minimum.bioassay.survival, 
-                                                maximum.bioassay.survival, 
-                                                divisions){
+                                                nsim = 1000, 
+                                                minimum.resistance.value = 0, 
+                                                maximum.resistance.value = 25000,
+                                                minimum.bioassay.survival = 0, 
+                                                maximum.bioassay.survival = 1, 
+                                                divisions = 0.01){
   
     df=data.frame(bioassay.survival.values=seq(minimum.bioassay.survival, maximum.bioassay.survival,
                                              by = divisions))%>%
