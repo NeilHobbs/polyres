1bb08309d03265e3cee87a303a95dabab2604256
NeilHobbs <neilphobbs@hotmail.com>
2020-05-22 14:20:36 +0100

    Corrected for missing 10/R

--- R/plot_log_response_to_insecticide_selection.R
+++ R/plot_log_response_to_insecticide_selection.R
@@ -7,14 +7,16 @@
 
 plot_log_response_to_insecticide_selection = function(response.values){
   
+  response.values = 10/response.values
+  
   response.values = log10(response.values)
   
   #Coerce into a dataframe to allow for plotting
   output=data.frame(response.values) 
   
   #Histogram of years
-  selection.histogram =  ggplot(data = output, aes(x=output)) +
-    geom_histogram(fill = "grey", colour = "black") +
+  selection.histogram =  ggplot(data = output, aes(x=response.values)) +
+    geom_histogram(fill = "grey", colour = "black", binwidth = 0.1) +
     xlab("log(Years to 10% Survival)") +
     theme_classic()
   
