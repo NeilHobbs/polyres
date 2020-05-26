af6dd2226db00f86a5dd8cc4b24a5d7e315c1e26
NeilHobbs <neilphobbs@hotmail.com>
2020-05-26 13:08:37 +0100

    allows the user to define number of insecticides

---
+++ R/set_number_of_insecticides.R
@@ -0,0 +1,16 @@
+#' This function sets the number of insecticides to be used in the simulation
+#' 
+#' @param total.insecticides This is the total number of insecticides available for deployment
+
+set_number_of_insecticides = function(total.insecticides = 3){
+  
+  number.of.insecticides = seq(1,total.insecticides, by = 1)
+  
+}
+
+
+
+
+
+
+
