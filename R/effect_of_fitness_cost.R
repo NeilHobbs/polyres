b96c5efb28f58bd0a970980bb1e75f6052e59c29
NeilHobbs <neilphobbs@hotmail.com>
2020-05-26 11:45:31 +0100

    Incorporate insecticide_selection function

--- R/effect_of_fitness_cost.R
+++ R/effect_of_fitness_cost.R
@@ -8,9 +8,24 @@
 
 #[NB set a check in the code so that z_bar cannot fall below 0 when the insecticide is not being deployed] 
 
-effect_of_fitness_cost = function(resistance.cost, resistance.selection.response){
+effect_of_fitness_cost = function(resistance.cost,
+                                  exposure.scaling.factor = 10,
+                                  nsim = 1000, 
+                                  minimum.insecticide.resistance.hertitability = 0.05, 
+                                  maximum.insecticide.resistance.hertitability = 0.30,
+                                  minimum.male.insecticide.exposure = 0,
+                                  maximum.male.insecticide.exposure = 1, 
+                                  minimum.female.insecticide.exposure = 0.4, 
+                                  maximum.female.insecticide.exposure = 0.9){
   
-  fitness.cost = (-resistance.cost) * resistance.selection.response
+  fitness.cost = (-resistance.cost) * response_to_insecticide_selection(exposure.scaling.factor = exposure.scaling.factor,
+                                                                        nsim = nsim, 
+                                                                        minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability, 
+                                                                        maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
+                                                                        minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
+                                                                        maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
+                                                                        minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
+                                                                        maximum.female.insecticide.exposure = maximum.female.insecticide.exposure)
   
   return(fitness.cost)
   
