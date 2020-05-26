5816fdb4eb8edfcd614d467b16f9cca77c074878
NeilHobbs <neilphobbs@hotmail.com>
2020-05-26 16:04:46 +0100

    Functions that check resistance levels

---
+++ R/check_resistance_10.R
@@ -0,0 +1,16 @@
+#' To see if resistance is gives lower than 10% survival.
+
+
+check_resistance = function(current.resistance.intensity){
+  
+  if(current.resistance.intensity > bioassay_survival_to_resistance(maximum.bioassay.survival.proportion = 1,
+                                                                    michaelis.menten.slope = 1, 
+                                                                    half.population.bioassay.survival.resistance = 900, 
+                                                                    bioassay.survival = 0.1, 
+                                                                    estimate.precision = 0.01, 
+                                                                    sd.population.resistance = 0,
+                                                                    nsim = 1000,
+                                                                    minimum.resistance.value = 0, 
+                                                                    maximum.resistance.value = 25000)){FALSE} else(TRUE)
+  
+}
---
+++ R/check_resistance_5.R
@@ -0,0 +1,18 @@
+#' Can the insecticide be used???
+#' 
+#' if true can be used
+
+
+check_resistance = function(current.resistance.intensity){
+  
+  if(current.resistance.intensity > bioassay_survival_to_resistance(maximum.bioassay.survival.proportion = 1,
+                                    michaelis.menten.slope = 1, 
+                                    half.population.bioassay.survival.resistance = 900, 
+                                    bioassay.survival = 0.05, 
+                                    estimate.precision = 0.01, 
+                                    sd.population.resistance = 0,
+                                    nsim = 1000,
+                                    minimum.resistance.value = 0, 
+                                    maximum.resistance.value = 25000)){FALSE} else(TRUE)
+  
+}
