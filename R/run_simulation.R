6f4f076a8f80f96907a36f396167a24f92de9cb6
NeilHobbs <neilphobbs@hotmail.com>
2020-05-28 15:44:10 +0100

    Working... But odd refugia results

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -18,115 +18,139 @@
 #' @param irm.strategy To be able to set the resistance management strategy (rotations, sequences)
 
 
-#And need to figure out how to save into an array!
+#Notes:
+    #There does not appear to be migration to the refugia from the treatment site, as refugia values remain 0.00000. 
 
-run_simulation = function(number.of.insecticides = 1,
-                                       exposure.scaling.factor = 10,
-                                       nsim = 1000, 
-                                       minimum.insecticide.resistance.hertitability = 0.05, 
-                                       maximum.insecticide.resistance.hertitability = 0.30,
-                                       minimum.male.insecticide.exposure = 0,
-                                       maximum.male.insecticide.exposure = 1, 
-                                       minimum.female.insecticide.exposure = 0.4, 
-                                       maximum.female.insecticide.exposure = 0.9,
-                                       resistance.cost,
-                                       initial.resistance.intensity,
-                                       min.intervention.coverage = 0.1, 
-                                       max.intervention.coverage = 0.9, 
-                                       initial.refugia.resistance,
-                                       min.dispersal.rate = 0.1,
-                                       max.dispersal.rate = 0.9
-                                       #irm.strategy
-                                       ){
-  
-  count.insecticides = set_number_of_insecticides(number.of.insecticides)
-  
-  # Set up the array for the data:
-  #allow for 500 generations (50 years) with rows.: How to store the data into the array?
-  #Columns: generation, treatment.site IR, refugia IR, insecticide.deployed. 
-  simulation.data = array(c(500, 4, length(count.insecticides)))
-  
-  
-  ##Can be either rotation or sequence
-  #strategy = irm.strategy
-  
-  #set starting intensities to zero.
-  track.mean.site.resistance = 0
-  
-  for(year in 1:50){
+
+ #And need to figure out how to save into an array!
+
+ run_simulation = function(number.of.insecticides = 2,
+                                        exposure.scaling.factor = 10,
+                                        nsim = 1000,
+                                        minimum.insecticide.resistance.hertitability = 0.05,
+                                        maximum.insecticide.resistance.hertitability = 0.30,
+                                        minimum.male.insecticide.exposure = 0,
+                                        maximum.male.insecticide.exposure = 1,
+                                        minimum.female.insecticide.exposure = 0.4,
+                                        maximum.female.insecticide.exposure = 0.9,
+                                        resistance.cost,
+                                        starting.treatment.site.intensity = 0,
+                                        starting.refugia.intensity = 0,
+                                        min.intervention.coverage = 0.1,
+                                        max.intervention.coverage = 0.9,
+                                        min.dispersal.rate = 0.1,
+                                        max.dispersal.rate = 0.9,
+                                        #irm.strategy,
+                                        maximum.generations = 500
+                                        ){
+      
+   
+    #Start by creating an array:
+    #dimension 1: site = c("refugia", "treatment")
+    #dimension 2: insectide
+    #dimension 3: generation.
+    #refugia is the refugia. treatment is the place where insecticides are the intervention site where insecticides are deployed.
+   
+       sim.array  = create_starting_array(n.insecticides = number.of.insecticides, maximum.generations = maximum.generations)
     
-    #select an insecticide
-     for(insecticide in 1:length(count.insecticides)){
+    
+    #Set starting resistance intensities (fills in whole column with zeroes) - shouldn't be an issue as updated in for loops:
+                #treatment(intervention) site
+      sim.array['refugia', , ] = starting.refugia.intensity
       
-      #Check if survival/resistance less than the limits of 10% survival. #How to deploy only 1 insecticide at a time??
-      if(check_resistance_10(current.resistance.status = track.mean.site.resistance[insecticide]) == TRUE){
-        deploy.insecticide = TRUE} else (deploy.insecticide = FALSE)
+                #refugia site
+      sim.array['treatment', , ] = starting.treatment.site.intensity 
       
-      #can insecticide be used? IR level less than 47 (5% survival) to be re-used. If TRUE can be re-used
-      #check_resistance_5(current.resistance.status = track.mean.site.resistance)
-      
-       ##then run all the generations of a year with each insecticide
-      for(generation in 1:10){
-        ##Incorporate 
-        track.mean.site.resistance[generation + 1] = if(deploy.insecticide == TRUE){ #or [generation] = function[generation - 1]?
-                   mean(insecticide_deployed_migration(
+    
+   ##Define which insecticide resistance management is being used: Can be either rotation or sequence
+   #strategy = irm.strategy
+
+    #start at generation 2, as generation 1 has intensities set.
+   for(generation in 2:maximum.generations){
+
+     #select an insecticide
+      for(insecticide in 1:number.of.insecticides){
+
+       #Check if survival/resistance less than the limits of 10% survival. #How to deploy only 1 insecticide at a time??
+       #if(check_resistance_10(current.resistance.status = sim.array['treatment', insecticide, generation - 1]) == TRUE){
+        #deploy.insecticide = TRUE} else (deploy.insecticide = FALSE)
+        
+        
+        ##This is just to make the simulation run for checking
+        deploy.insecticide = TRUE
+        
+       #can insecticide be used? IR level less than 47 (5% survival) to be re-used. If TRUE can be re-used
+       #check_resistance_5(current.resistance.status = track.mean.site.resistance)
+
+         #Do refugia first, updating each generation for each insecticide
+          #calculate the population mean from the previous population mean
+        sim.array['refugia', insecticide, generation] = mean(refugia_migration_effect(
+                                          initial.refugia.resistance = sim.array['refugia', insecticide, generation - 1],
+                                          resistance.cost = resistance.cost,
+                                          exposure.scaling.factor = exposure.scaling.factor,
+                                          nsim = nsim, 
+                                          minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability, 
+                                          maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
+                                          minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
+                                          maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
+                                          minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
+                                          maximum.female.insecticide.exposure = maximum.female.insecticide.exposure,
+                                          min.intervention.coverage = min.intervention.coverage, 
+                                          max.intervention.coverage = max.intervention.coverage, 
+                                          min.dispersal.rate = min.dispersal.rate,
+                                          max.dispersal.rate = max.dispersal.rate)) #end of refugia
+        
+        #next do treatment site: requires checking if insecticide deployed or not (will be TRUE/FALSE)
+          
+          
+         ##Incorporate
+         sim.array['treatment', insecticide, generation] = if(deploy.insecticide == TRUE){#Insecticide is deployed in treatment site
+                   #calculate population mean from previous population mean when insecticide present
+                    mean(insecticide_deployed_migration(
+               exposure.scaling.factor = exposure.scaling.factor,
+               nsim = nsim,
+               minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability,
+               maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
+               minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
+               maximum.male.insecticide.exposure = maximum.male.insecticide.exposure,
+               minimum.female.insecticide.exposure = minimum.female.insecticide.exposure,
+               maximum.female.insecticide.exposure = maximum.female.insecticide.exposure,
+               resistance.cost = resistance.cost,
+               initial.resistance.intensity = sim.array['treatment', insecticide, generation - 1],
+               min.intervention.coverage = min.intervention.coverage,
+               max.intervention.coverage = max.intervention.coverage,
+               initial.refugia.resistance = sim.array['refugia', insecticide, generation - 1],
+               min.dispersal.rate = min.dispersal.rate,
+               max.dispersal.rate = max.dispersal.rate))} #end of insecticide deployed
+                                                      else( #insecticide is not deployed
+                      #calculate population mean when insecticide not deployed from previous population mean
+                    mean(insecticide_not_deployed_migration(
+              initial.resistance.intensity = sim.array['treatment', insecticide, generation - 1],
+              resistance.cost = resistance.cost,
+              initial.refugia.resistance = sim.array['refugia', insecticide, generation - 1],
               exposure.scaling.factor = exposure.scaling.factor,
-              nsim = nsim, 
-              minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability, 
+              nsim = nsim,
+              minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability,
               maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
               minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
-              maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
-              minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
+              maximum.male.insecticide.exposure = maximum.male.insecticide.exposure,
+              minimum.female.insecticide.exposure = minimum.female.insecticide.exposure,
               maximum.female.insecticide.exposure = maximum.female.insecticide.exposure,
-              resistance.cost = resistance.cost,
-              initial.resistance.intensity = track.mean.site.resistance[generation],
-              min.intervention.coverage = min.intervention.coverage, 
-              max.intervention.coverage = max.intervention.coverage, 
-              initial.refugia.resistance = track.mean.refugia.resistance[generation],
+              min.intervention.coverage = min.intervention.coverage,
+              max.intervention.coverage = max.intervention.coverage,
               min.dispersal.rate = min.dispersal.rate,
-              max.dispersal.rate = max.dispersal.rate))} else(
-         
-                   mean(insecticide_not_deployed_migration(
-             initial.resistance.intensity,
-             resistance.cost,
-             initial.refugia.resistance = track.mean.site.resistance[generation], #or [generation - 1?, with [generation] as the target]
-             exposure.scaling.factor = exposure.scaling.factor,
-             nsim = nsim, 
-             minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability, 
-             maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
-             minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
-             maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
-             minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
-             maximum.female.insecticide.exposure = maximum.female.insecticide.exposure, 
-             min.intervention.coverage = min.intervention.coverage, 
-             max.intervention.coverage = max.intervention.coverage, 
-             min.dispersal.rate = min.dispersal.rate,
-             max.dispersal.rate = max.dispersal.rate
-           )))
-        
-           track.mean.refugia.resistance[generation + 1] = mean(refugia_migration_effect(
-                                            initial.refugia.resistance = track.mean.refugia.resistance[generation],
-                                            resistance.cost = resistance.cost,
-                                            exposure.scaling.factor = exposure.scaling.factor,
-                                            nsim = nsim, 
-                                            minimum.insecticide.resistance.hertitability = minimum.insecticide.resistance.hertitability, 
-                                            maximum.insecticide.resistance.hertitability = maximum.insecticide.resistance.hertitability,
-                                            minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
-                                            maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
-                                            minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
-                                            maximum.female.insecticide.exposure = maximum.female.insecticide.exposure,
-                                            min.intervention.coverage = min.intervention.coverage, 
-                                            max.intervention.coverage = max.intervention.coverage, 
-                                            min.dispersal.rate = min.dispersal.rate,
-                                            max.dispersal.rate = max.dispersal.rate))
-           #end of for(generation) loop
-           }
-    
-    #return the mean population IR each year.
-  return(track.mean.site.resistance[year])
-  }
-}
-}
+              max.dispersal.rate = max.dispersal.rate
+            )))
+
+           }#end of insecticide loop
+
+     #return the mean population IR each year.
+   
+   }#end of for(generation) loop
+      
+      #ensure the simulation array is return after running
+      return(sim.array)
+ }
 
 
  
