bc27ae78ca9182e43e476f3a41572ac6a0ac2613
NeilHobbs <neilphobbs@hotmail.com>
2020-10-21 15:38:30 +0100

    Added error messages

--- R/migration_refugia_to_treatment.R
+++ R/migration_refugia_to_treatment.R
@@ -1,5 +1,4 @@
-#'This function calculates the migration effect from the refugia to the treatment area.
-#'Equation 6B
+#'@title  calculate the migration effect from the refugia to the treatment area.
 #'
 #'@param min.intervention.coverage Minimum coverage of uniform distribution of insecticide intervention, set to 0.1
 #'@param max.intervention.coverage Minimum coverage of uniform distribution of insecticide intervention, set to 0.9
@@ -7,7 +6,7 @@
 #'@param max.dispersal.rate Maximum dispersal propotion of unifrom distribution, set to 0.9
 #'@param nsim Number of interations of the runif function 
 #'
-#'@return exchange.refugia.to.treatment
+#'@return exchange.refugia.to.treatment A vector of length nsim
 
 migration_refugia_to_treatment = function(nsim = 1000, 
                                           min.intervention.coverage = 0.1, 
@@ -15,9 +14,20 @@ migration_refugia_to_treatment = function(nsim = 1000,
                                           min.dispersal.rate = 0.1,
                                           max.dispersal.rate = 0.9){
   
+  #Error messages to prevent incorrect parameter values being used. 
+  if(0 > min.intervention.coverage | min.intervention.coverage > 1){stop("min.intervention.coverage must be between 0 and 1")}
+  if(0 > max.intervention.coverage | max.intervention.coverage > 1){stop("max.intervention.coverage must be between 0 and 1")}
+  if(min.intervention.coverage > max.intervention.coverage){stop("min.intervention.coverage is greater than max.intervention.coverage")}
+
+  if(0 > min.dispersal.rate | min.dispersal.rate > 1){stop("min.dispersal.rate must be between 0 and 1")}
+  if(0 > max.dispersal.rate | max.dispersal.rate > 1){stop("max.dispersal.rate must be between 0 and 1")}
+  if(min.dispersal.rate > max.dispersal.rate){stop("min.dispersal.rate is greater than max.dispersal.rate")}
+  
   exchange.refugia.to.treatment = runif(nsim, min = min.intervention.coverage, max = max.intervention.coverage) * 
                                      runif(nsim, min = min.dispersal.rate, max= max.dispersal.rate)
   
   return(exchange.refugia.to.treatment)
   
 }
+
+#This is equation 6B in the MS
--- R/migration_treatment_to_refugia.R
+++ R/migration_treatment_to_refugia.R
@@ -1,5 +1,4 @@
-#'This function calculates the migration effect from the treatment area to the refugia.
-#'Equation 6A
+#'@title Calculate the migration effect from the treatment area to the refugia.
 #'
 #'@param min.intervention.coverage Minimum coverage of uniform distribution of insecticide intervention, set to 0.1
 #'@param max.intervention.coverage Minimum coverage of uniform distribution of insecticide intervention, set to 0.9
@@ -15,6 +14,16 @@ migration_treatment_to_refugia = function(nsim = 1000,
                                           min.dispersal.rate = 0.1,
                                           max.dispersal.rate = 0.9){
   
+  #Error Messages:
+  if(0 > min.intervention.coverage | min.intervention.coverage > 1){stop("min.intervention.coverage must be between 0 and 1")}
+  if(0 > max.intervention.coverage | max.intervention.coverage > 1){stop("max.intervention.coverage must be between 0 and 1")}
+  if(min.intervention.coverage > max.intervention.coverage){stop("min.intervention.coverage is greater than max.intervention.coverage")}
+
+  if(0 > min.dispersal.rate | min.dispersal.rate > 1){stop("min.dispersal.rate must be between 0 and 1")}
+  if(0 > max.dispersal.rate | max.dispersal.rate > 1){stop("max.dispersal.rate must be between 0 and 1")}
+  if(min.dispersal.rate > max.dispersal.rate){stop("min.dispersal.rate is greater than max.dispersal.rate")}
+
+  
   exchange.treatment.to.refugia = (1 - runif(nsim, 
                                              min = min.intervention.coverage,
                                              max = max.intervention.coverage)) * 
@@ -24,3 +33,5 @@ migration_treatment_to_refugia = function(nsim = 1000,
   
   return(exchange.treatment.to.refugia)
 }
+
+#This is equation 6A in the MS
