1b27321e1b1b1cf8600c18967f62d4b22ee3ca4e
NeilHobbs <neilphobbs@hotmail.com>
2020-10-28 11:19:56 +0000

    Title, Description, Annotations. unique() to prevent duplicates

--- R/return_and_withdrawal_of_insecticides.R
+++ R/return_and_withdrawal_of_insecticides.R
@@ -1,6 +1,9 @@
-#Function to implement withdrawal and return of insecticides. This returns a list of the the
-# available[[1]] and withdrawn[[2]] insecticides.
-
+#' @title Implement both the withdrawal and return of insecticides from the arsenal. 
+#' 
+#' @description This function determines both which insecticides are available for deployment and those that 
+#' need to be withdrawn from the arsenal. Currently this function is designed around only a single insecticide
+#' deployment, and may need modifying to account for insecticide mixtures. 
+#' 
 #' @param number.of.insecticides = The total number of insecticides in the simulation
 #' @param current.generation = The current generation in the simualation
 #' @param withdrawal.threshold = The resistance intensity threshold for withdrawing an insecticide from the arsenal
@@ -8,7 +11,9 @@
 #' @param simulation.array = The array holding the simulation results
 #' @param available.vector = A vector containing the previously available insecticides
 #' @param withdrawn.vector = A vector containing the previously withdrawn insecticides
-
+#' 
+#'@return list(available.vector.r2, withdrawn.vector.r2) returns a list of the the updated
+#' available[[1]] and withdrawn[[2]] insecticides.
 
 return_and_withdrawal_of_insecticides = function(number.of.insecticides,
                                                  current.generation,
@@ -24,11 +29,13 @@ return_and_withdrawal_of_insecticides = function(number.of.insecticides,
                                                                         simulation.array = simulation.array,
                                                                         available.vector = available.vector,
                                                                         withdrawn.vector = withdrawn.vector)
-  
+
   #Convert the lists back to the vectors
-  available.vector.r1 = list.available.withdrawn.r1[[1]]
-  withdrawn.vector.r1 = list.available.withdrawn.r1[[2]]
-  
+        #unique function included to prevent any duplicates arising. This should not
+          #impact on the correctness of the allocation
+  available.vector.r1 = unique(list.available.withdrawn.r1[[1]])
+  withdrawn.vector.r1 = unique(list.available.withdrawn.r1[[2]])
+
   #second do return:
   list.available.withdrawn.r2 = return_of_insecticides_to_arsenal(number.of.insecticides = number.of.insecticides,
                                                                   current.generation = current.generation,
@@ -36,6 +43,9 @@ return_and_withdrawal_of_insecticides = function(number.of.insecticides,
                                                                   simulation.array = simulation.array,
                                                                   available.vector = available.vector.r1,
                                                                   withdrawn.vector = withdrawn.vector.r1)
-  
-  return(list.available.withdrawn.r2)
+
+  available.vector.r2 = unique(list.available.withdrawn.r2[[1]])
+  withdrawn.vector.r2 = unique(list.available.withdrawn.r2[[2]])
+
+  return(list(available.vector.r2, withdrawn.vector.r2))
 }
