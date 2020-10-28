d21da645d584ead8f37b7fe8c35e27a7fa652c40
NeilHobbs <neilphobbs@hotmail.com>
2020-10-28 11:40:36 +0000

    Need consider what outcomes (primary and secondary); would be good to know when parameter space testing.

---
+++ R/get_simulation_outcomes.R
@@ -0,0 +1,16 @@
+#' 
+#' 
+#' 
+
+
+get_simulation_outcomes = function(){
+  
+  #1. Duration of Intervention (in generations)
+  #2. Peak Resistance
+  #3. Average (mean) Resistance to deployed insecticide
+  #4. Number of generations where the insecticide was deployed above the exceedance threshold
+  #5. Number of generations where the insecticide was above the exceedance threshold (deployed and not deployed)
+  #6. Rolling Average (mean) Resistance to deployed insecticide:
+      # 0 - 100 ; 100 - 200 ; 200 - 300 ; 300 - 400; 400-500.
+  
+}
