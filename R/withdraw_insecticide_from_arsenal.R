46f8ed77b790b8a9c76e9ab5c628d1ea0364e646
NeilHobbs <neilphobbs@hotmail.com>
2020-10-28 11:27:48 +0000

    Updated title

--- R/withdraw_insecticide_from_arsenal.R
+++ R/withdraw_insecticide_from_arsenal.R
@@ -1,5 +1,5 @@
-#A function that removes an insecticide from the arsenal. This function updates the withdrawn and available insecticide 
-#vectors. Returns a list: [[1]]available vector and [[2]]withdrawn vector
+#'@title Remove an insecticide from the arsenal. This function updates the withdrawn and available insecticide
+#' vectors. Returns a list: [[1]]available vector and [[2]]withdrawn vector.
 
 #' @param withdrawn.vector = A vector containing the insecticides with have been withdrawn from the arsenal.
 #' @param available.vector = A vector containing the insecticides which are available for deployment.
@@ -9,10 +9,10 @@
 withdraw_insecticide_from_arsenal = function(withdrawn.vector,
                                              available.vector,
                                              insecticide.to.withdraw){
-  
+
   available.vector.updated = available.vector[!available.vector %in% insecticide.to.withdraw]
-  withdrawn.vector.updated = c(withdrawn.vector, insecticide.to.withdraw)  
-  
+  withdrawn.vector.updated = c(withdrawn.vector, insecticide.to.withdraw)
+
   return(list(available.vector.updated, withdrawn.vector.updated)) #[[1]]available vector ; [[2]]withdrawn vector
-  
+
 }
