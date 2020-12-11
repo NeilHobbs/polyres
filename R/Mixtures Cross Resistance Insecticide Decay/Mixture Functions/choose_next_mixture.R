a64326b216525cd4eb3883da06e6c8ea3bdc46b5
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:14:16 +0000

    added roxygen comments

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/choose_next_mixture.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/choose_next_mixture.R
@@ -1,3 +1,11 @@
+#' @title Choose the next mixture.
+#' 
+#' @description Choose the next available mixture for deployment.
+#' 
+#' @param previous.mixture = The mixture that was last deployed
+#' @param total.mixtures = The total number of mixtures in the simulation
+#' @param available.mixtures = The mixtures that are available for deployment (both parts of the mixture are in the armoury)
+
 choose_next_mixture = function(previous.mixture,
                                total.mixtures,
                                available.mixtures){
