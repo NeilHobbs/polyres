5e3b4b1f8d5e2728f6b58a6453e22a6adf06f7dc
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:20:16 +0000

    roxygen comments.
    corrected inputs

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/make_pyrethroid_mixtures.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/make_pyrethroid_mixtures.R
@@ -1,10 +1,13 @@
-
-
+#' @title Make all possible combinations of mixtures
+#'
+#' @description This function pairs all insecticides with all other insecticides.
+#' 
+#' @param number.of.insecticides = The total number of insecticides in the simulation.
 
 make_pyrethroid_mixtures = function(number.of.insecticides){
   
  
-  mixture.part.1 = rep(1, times = length(non.pyrethroid))
+  mixture.part.1 = rep(1, times = length(number.of.insecticides - 1))
   mixture.part.2 = seq(from = 2, to = number.of.insecticides, by = 1)
   mixture.id = seq(from = 1, to = length(mixture.part.1), by = 1)
    
