c47cdec03edef76a3386cdd756e11d42b18925c7
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:13:03 +0000

    changes of input values

--- R/Mixtures Cross Resistance Insecticide Decay/Cross Selection Functions/Updated/testing no cross resistance gives the same results.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Cross Selection Functions/Updated/testing no cross resistance gives the same results.R
@@ -1,26 +1,26 @@
 A = get_simulation_dataframe(run_simulation_intervention(number.of.insecticides = 3,
-                                                              exposure.scaling.factor = 10,
-                                                              nsim = 1,
-                                                              minimum.insecticide.resistance.heritability = 0.3,
-                                                              maximum.insecticide.resistance.heritability = 0.3,
-                                                              minimum.male.insecticide.exposure = 0.5,
-                                                              maximum.male.insecticide.exposure = 0.5,
-                                                              minimum.female.insecticide.exposure = 0.8,
-                                                              maximum.female.insecticide.exposure = 0.8,
-                                                              resistance.cost = 0.2,
-                                                              starting.treatment.site.intensity = 0,
-                                                              starting.refugia.intensity = 0,
-                                                              min.intervention.coverage = 0.7,
-                                                              max.intervention.coverage = 0.7,
-                                                              min.dispersal.rate = 0.3,
-                                                              max.dispersal.rate = 0.3,
-                                                              maximum.generations = 500,
-                                                              irm.strategy = "sequence", #will be sequence or rotation (plus mixture later on),
-                                                              half.population.bioassay.survival.resistance = 900,
-                                                              withdrawal.threshold.value = 0.1, #this is the survival proportion in a bioassay that would withdraw the insecticide from the arsenal
-                                                              return.threshold.value = 0.05, #this is the survival proportion in a bioassay that would return insecticide to arsenal
-                                                              deployment.frequency = 10, #Number of mosquito generations between choosing insecticides (note, 1 year is 10 generations)
-                                                              maximum.resistance.value = 25000), 
+                                                         exposure.scaling.factor = 10,
+                                                         nsim = 1,
+                                                         minimum.insecticide.resistance.heritability = 0.3,
+                                                         maximum.insecticide.resistance.heritability = 0.3,
+                                                         minimum.male.insecticide.exposure = 0.5,
+                                                         maximum.male.insecticide.exposure = 0.5,
+                                                         minimum.female.insecticide.exposure = 0.8,
+                                                         maximum.female.insecticide.exposure = 0.8,
+                                                         resistance.cost = 0.2,
+                                                         starting.treatment.site.intensity = 0,
+                                                         starting.refugia.intensity = 0,
+                                                         min.intervention.coverage = 0.7,
+                                                         max.intervention.coverage = 0.7,
+                                                         min.dispersal.rate = 0.3,
+                                                         max.dispersal.rate = 0.3,
+                                                         maximum.generations = 500,
+                                                         irm.strategy = "sequence", #will be sequence or rotation (plus mixture later on),
+                                                         half.population.bioassay.survival.resistance = 900,
+                                                         withdrawal.threshold.value = 0.1, #this is the survival proportion in a bioassay that would withdraw the insecticide from the arsenal
+                                                         return.threshold.value = 0.05, #this is the survival proportion in a bioassay that would return insecticide to arsenal
+                                                         deployment.frequency = 10, #Number of mosquito generations between choosing insecticides (note, 1 year is 10 generations)
+                                                         maximum.resistance.value = 25000), 
                              number.of.insecticides = 3,
                              maximum.generations = 500)
   
@@ -32,34 +32,34 @@ plot_simulation(A,
 
 
 
-B =get_simulation_dataframe(run_simulation_intervention_cross_selection_test(number.of.insecticides = 5,
-                                                                             exposure.scaling.factor = 10,
-                                                                             nsim = 1,
-                                                                             minimum.insecticide.resistance.heritability = 0.3,
-                                                                             maximum.insecticide.resistance.heritability = 0.3,
-                                                                             minimum.male.insecticide.exposure = 1,
-                                                                             maximum.male.insecticide.exposure = 1,
-                                                                             minimum.female.insecticide.exposure = 1,
-                                                                             maximum.female.insecticide.exposure = 1,
-                                                                             resistance.cost = 0.1,
-                                                                             starting.treatment.site.intensity = 0,
-                                                                             starting.refugia.intensity = 0,
-                                                                             min.intervention.coverage = 1,
-                                                                             max.intervention.coverage = 1,
-                                                                             min.dispersal.rate = 0.1,
-                                                                             max.dispersal.rate = 0.1,
-                                                                             maximum.generations = 500,
-                                                                             irm.strategy = "rotation", #will be sequence or rotation (plus mixture later on),
-                                                                             half.population.bioassay.survival.resistance = 900,
-                                                                             withdrawal.threshold.value = 0.1, #this is the survival proportion in a bioassay that would withdraw the insecticide from the arsenal
-                                                                             return.threshold.value = 0.05, #this is the survival proportion in a bioassay that would return insecticide to arsenal
-                                                                             deployment.frequency = 10, #Number of mosquito generations between choosing insecticides (note, 1 year is 10 generations)
-                                                                             maximum.resistance.value = 25000,
-                                                                             min.cross.selection = 0,
-                                                                             max.cross.selection = 0),
-                            number.of.insecticides = 5,
+B =get_simulation_dataframe(run_simulation_intervention_cross_selection(number.of.insecticides = 3,
+                                                                        exposure.scaling.factor = 10,
+                                                                        nsim = 1,
+                                                                        minimum.insecticide.resistance.heritability = 0.3,
+                                                                        maximum.insecticide.resistance.heritability = 0.3,
+                                                                        minimum.male.insecticide.exposure = 0.5,
+                                                                        maximum.male.insecticide.exposure = 0.5,
+                                                                        minimum.female.insecticide.exposure = 0.8,
+                                                                        maximum.female.insecticide.exposure = 0.8,
+                                                                        resistance.cost = 0.2,
+                                                                        starting.treatment.site.intensity = 0,
+                                                                        starting.refugia.intensity = 0,
+                                                                        min.intervention.coverage = 0.7,
+                                                                        max.intervention.coverage = 0.7,
+                                                                        min.dispersal.rate = 0.3,
+                                                                        max.dispersal.rate = 0.3,
+                                                                        maximum.generations = 500,
+                                                                        irm.strategy = "sequence", #will be sequence or rotation (plus mixture later on),
+                                                                        half.population.bioassay.survival.resistance = 900,
+                                                                        withdrawal.threshold.value = 0.1, #this is the survival proportion in a bioassay that would withdraw the insecticide from the arsenal
+                                                                        return.threshold.value = 0.05, #this is the survival proportion in a bioassay that would return insecticide to arsenal
+                                                                        deployment.frequency = 10, #Number of mosquito generations between choosing insecticides (note, 1 year is 10 generations)
+                                                                        maximum.resistance.value = 25000,
+                                                                        min.cross.selection = 0,
+                                                                        max.cross.selection = 0),
+                            number.of.insecticides = 3,
                             maximum.generations = 500)  
-  
+
 
 
 plot_simulation(B,
@@ -68,4 +68,3 @@ plot_simulation(B,
                 return.threshold = 0.05)
 
 
-#Issue is with dispersal.rate
