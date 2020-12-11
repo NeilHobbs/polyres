75ec08320dbca13075303ac829559caac4fe1b60
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:14:55 +0000

    corrected deployment.frequency; added roxygen comments

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/deploy_mixture.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/deploy_mixture.R
@@ -1,11 +1,20 @@
+#' @title Deploy the Insecticidal Mixture
+#' 
+#' @description Deploys the candidate mixture by adding to the deployment dataframe.
+#' 
+#' @param candidate.mixture.id = The mixture.id of the candidate mixture to be deployed.
+#' @param mixture.df = The dataframe containing all the mixture combinations and IDs.
+#' @param deployment.df = The dataframe that holds the deployment information.
+#' @param deployment.frequency = The duration of a single insecticidal deployment in generations.
+
 deploy_mixture= function(candidate.mixture.id,
                          mixture.df,
                          deployment.df,
-                         freq.deployment){
+                         deployment.frequency){
   
-  mixture.id =  rep((mixture.df[candidate.mixture.id, 1]), times = freq.deployment)
-  mixture.part.1 = rep((mixture.df[candidate.mixture.id, 2]), times = freq.deployment)
-  mixture.part.2 = rep((mixture.df[candidate.mixture.id, 3]), times = freq.deployment)
+  mixture.id =  rep((mixture.df[candidate.mixture.id, 1]), times = deployment.frequency)
+  mixture.part.1 = rep((mixture.df[candidate.mixture.id, 2]), times = deployment.frequency)
+  mixture.part.2 = rep((mixture.df[candidate.mixture.id, 3]), times = deployment.frequency)
   
   mixture.to.deploy = data.frame(mixture.id, mixture.part.1, mixture.part.2)
   
