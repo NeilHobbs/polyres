edcd470461b3d231145bbae5f8de21fe2585f471
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:19:04 +0000

    roxygen comments

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/make_mixture_sequential_discrete.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/make_mixture_sequential_discrete.R
@@ -1,4 +1,10 @@
-#This function makes mixture pairs that are discrete: eg. 1,2 ; 3,4 etc; will only work if number.of.insecticides is EVEN.
+#' @title Make discrete mixtures
+#' 
+#' @description This function makes mixture pairs that are discrete: eg. 1,2 ; 3,4 etc. 
+#' Will only work if number.of.insecticides is EVEN.
+#' 
+#' @param number.of.insecticides = The total number of insecticides included in the simulation.
+
 make_mixture_sequential_discrete = function(number.of.insecticides){
   
   mixture.part.1 = seq(from = 1, to = number.of.insecticides, by = 2)#odd numbered insecticides
