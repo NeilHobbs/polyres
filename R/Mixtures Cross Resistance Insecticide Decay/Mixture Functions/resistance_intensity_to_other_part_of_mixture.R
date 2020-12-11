8bbce0c55bba11d46b38f3cca12a5dd95b0c52bd
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:27:06 +0000

    made code tidier

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/resistance_intensity_to_other_part_of_mixture.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/resistance_intensity_to_other_part_of_mixture.R
@@ -3,19 +3,23 @@
 #' @description The purpose of this function is to find the bioassay survival to the second part of the insecticide mixture.
 
 resistance_intensity_to_other_part_of_mixture = function(deployed.mixture,
-                                             generation,
-                                             insecticide,
-                                             sim.array){
+                                                         generation,
+                                                         insecticide,
+                                                         sim.array){
   
   #if deployed.mixture.1 is being tracked; get resistance intensity to deployed.mixture.2
-if(insecticide == deployed.mixture$deployed.mixture.1[generation]){
-  resistance.intensity.to.other.insecticide = sim.array["treatment", deployed.mixture$deployed.mixture.2[generation - 1], generation -1]
-}
+  if(insecticide == deployed.mixture$mixture.part.1[generation]){
+    resistance.intensity.to.other.insecticide = sim.array["treatment", 
+                                                          deployed.mixture$mixture.part.2[generation - 1],
+                                                          generation -1]
+  }
   #if deployed.mixture.2 is being tracked; calculate survival from deployed.mixture.1
-if(insecticide == deployed.mixture$deployed.mixture.2[generation]){
-  resistance.intensity.to.other.insecticide =  sim.array["treatment", deployed.mixture$deployed.mixture.1[generation - 1], generation - 1]
-}
-                                                                        
+  if(insecticide == deployed.mixture$mixture.part.2[generation]){
+    resistance.intensity.to.other.insecticide =  sim.array["treatment", 
+                                                           deployed.mixture$mixture.part.1[generation - 1], 
+                                                           generation - 1]
+  }
+  
   return(resistance.intensity.to.other.insecticide) 
 }
 
