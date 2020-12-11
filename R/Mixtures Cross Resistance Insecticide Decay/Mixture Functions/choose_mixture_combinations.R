0a4e39d0d44107acbb238bd12a739c72c28627f7
NeilHobbs <neilphobbs@hotmail.com>
2020-12-11 15:13:45 +0000

    added roxygen comments

--- R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/choose_mixture_combinations.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Mixture Functions/choose_mixture_combinations.R
@@ -1,4 +1,13 @@
-
+#' @title Choose a subset of the total mixtures
+#' 
+#' @description This function selects a subset of the total mixtures that have been created. This is advantageous as
+#' allowing for all insecticides to be mixed (paired) with all other insecticides is likely to be an unrealistic scenario.
+#' However by randomly sampling a master dataframe of mixtures, an element of realism is implemented, as there will be random 
+#' samples which result in clusters of similar mixtures (which would be expectedd from similarities in residual decay rates and 
+#' ease of making mixtures out of particular insecticides).
+#' 
+#' @param number.of.mixtures = The total number of mixtures that are allowed to be made for use in the simulations.
+#' @param potential.mixtures = A master dataframe of mixtures that are to be sampled from. 
 
 choose_mixture_combinations = function(number.of.mixtures,
                                        potential.mixtures){
@@ -13,3 +22,6 @@ choose_mixture_combinations = function(number.of.mixtures,
   return(mixtures.df)
   
 }
+
+
+
