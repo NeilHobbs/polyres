a9bd4e87ba3733903bdac36dec5ce4e630d6dcd8
NeilHobbs <neilphobbs@hotmail.com>
2021-01-07 10:29:47 +0000

    Potentially a way to incorporate operational realism by having sampling accuracy

--- R/Mixtures Cross Resistance Insecticide Decay/Other/sample_for_bioassay_resistance.R
+++ R/Mixtures Cross Resistance Insecticide Decay/Other/sample_for_bioassay_resistance.R
@@ -2,25 +2,56 @@
   #the mosquito population.
 
 sample_for_bioassay_resistance = function(number.of.bioassays,
-                                          resistance.intensity,
-                                          standard.deviation){ 
+                                          resistance.intensity.intervention,
+                                          resistance.intensity.refugia,
+                                          standard.deviation,
+                                          intervention.coverage){ 
+  
+  number.intervention.bioassays = round((intervention.coverage * number.of.bioassays), digits = 0) 
+    
+  number.refugia.bioassays = round(((1 - intervention.coverage) * number.of.bioassays), digits = 0) 
+  
+  assays = number.intervention.bioassays + number.refugia.bioassays
   
   
-  bioassay.samples = rnorm(n = number.of.bioassays,
-                                mean = resistance.intensity,
+  number.intervention.bioassays = ifelse(assays < number.of.bioassays,
+                                    yes = (number.intervention.bioassays + 1), no = number.intervention.bioassays)
+  
+  
+   bioassay.samples.refugia = rnorm(n = number.refugia.bioassays,
+                                        mean = resistance.intensity.refugia,
+                                        sd = standard.deviation)
+
+
+
+  bioassay.samples.intervention = rnorm(n = number.intervention.bioassays,
+                                mean = resistance.intensity.intervention,
                                 sd = standard.deviation)
   
+  all.samples = c(bioassay.samples.intervention, bioassay.samples.refugia)
   
-  return(mean(bioassay.samples))
+  sampled.resistance = mean(all.samples)
+  
+  return(sampled.resistance)
   
 }
 
 
+A = sample_for_bioassay_resistance(number.of.bioassays = 17,
+                                   resistance.intensity.intervention = 60,
+                                   resistance.intensity.refugia = 50,
+                                   standard.deviation = 5,
+                                   intervention.coverage = 0.7)
+
+
+
 sample.values = c()
 for(i in 1:10000){
-sample.values[i] = sample_for_bioassay_resistance(n = 5,
-                               resistance.intensity = 100,
-                               standard.deviation = 10)
+sample.values[i] = sample_for_bioassay_resistance(number.of.bioassays = 17,
+                                                  resistance.intensity.intervention = 60,
+                                                  resistance.intensity.refugia = 50,
+                                                  standard.deviation = 5,
+                                                  intervention.coverage = 0.7)
 }
 
 
