7f855c7de63bbf158ac7f11fb141db2fc3d0ec44
NeilHobbs <neilphobbs@hotmail.com>
2021-04-28 13:33:21 +0100

    unit test

---
+++ Tests/testthat/test-find_available_mixtures.R
@@ -0,0 +1,16 @@
+test_that("withdraws all the mixtures", {
+  expect_equal(nrow(find_available_mixtures(mixture.df = make_mixture_sequential_discrete(4),
+                                       withdrawn.insecticides = c(1, 3))), 0)
+})
+
+test_that("returns only the available mixture", {
+  expect_equal(find_available_mixtures(mixture.df = make_mixture_sequential_discrete(4),
+                                       withdrawn.insecticides = c(1))[1 , 1], 2
+  )
+  expect_equal(find_available_mixtures(mixture.df = make_mixture_sequential_discrete(4),
+                                       withdrawn.insecticides = c(1))[1 , 2], 3
+  )
+  expect_equal(find_available_mixtures(mixture.df = make_mixture_sequential_discrete(4),
+                                       withdrawn.insecticides = c(1))[1 , 3], 4
+  )
+})
