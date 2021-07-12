70b6e28d9623e4086252dacfee5e756e8d49f045
NeilHobbs <neilphobbs@hotmail.com>
2021-07-12 11:06:17 +0100

    minor corrections

--- R/insecticide_deployed_indirect_cross_selection.R
+++ R/insecticide_deployed_indirect_cross_selection.R
@@ -57,16 +57,16 @@ insecticide_deployed_indirect_cross_selection  = function(currently.deployed.ins
                                                                  number.of.insecticides = number.of.insecticides,
                                                                  cross.selection.matrix = cross.selection.matrix,
                                                                  exposure.scaling.factor = exposure.scaling.factor,
-                                                                 nsim = nsim, 
-                                                                 minimum.insecticide.resistance.heritability = minimum.insecticide.resistance.heritability, 
+                                                                 nsim = nsim,
+                                                                 minimum.insecticide.resistance.heritability = minimum.insecticide.resistance.heritability,
                                                                  maximum.insecticide.resistance.heritability = maximum.insecticide.resistance.heritability,
                                                                  minimum.male.insecticide.exposure = minimum.male.insecticide.exposure,
-                                                                 maximum.male.insecticide.exposure = maximum.male.insecticide.exposure, 
-                                                                 minimum.female.insecticide.exposure = minimum.female.insecticide.exposure, 
+                                                                 maximum.male.insecticide.exposure = maximum.male.insecticide.exposure,
+                                                                 minimum.female.insecticide.exposure = minimum.female.insecticide.exposure,
                                                                  maximum.female.insecticide.exposure = maximum.female.insecticide.exposure,
                                                                  resistance.cost = resistance.cost)
-  
-  
+
+
   track.resistance.intensity = initial.resistance.intensity + (direct.selection.response + direct.selection.fitness.cost) + indirect.selection
   
   #Resistance intensity is not allowed to drop below zero.
